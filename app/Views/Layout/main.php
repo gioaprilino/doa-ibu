98c8b2e4729e3bc6d07dd0f564a149fbf71b31dd
Vaquos <furqonotong@gmail.com>
2023-11-05 07:15:07 +0700

    fix header

--- app/Views/Layout/main.php
+++ app/Views/Layout/main.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Bootstrap Components &rsaquo; Navbar &mdash; Stisla</title>
+    <title><?= $title; ?></title>
 
 
     <link rel="stylesheet" href="<?= base_url(); ?>assets/modules/bootstrap/css/bootstrap.min.css">
